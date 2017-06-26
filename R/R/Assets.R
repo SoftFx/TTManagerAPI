ecc1ffa3e86a60c8a3640fb1cb6fb9fa5f60f0dc
Yahor Mazko <yahor.mazko@soft-fx.lv>
2017-06-26 16:46:39 +0300

    Adding project files

---
+++ R/R/Assets.R
@@ -0,0 +1,37 @@
+#' Gets the Assets as requested
+#'
+#' @export
+ttmGetAssets <- function(accountId) {
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAssets',accountId)
+  GetAssetFrame()
+}
+#' Get Asset table
+GetAssetFrame<-function()
+{
+  Currency = GetAssetCurrency()
+  CurrencyId = GetAssetCurrencyId()
+  Amount = GetAssetAmount()
+  FreeAmount = GetAssetFreeAmount()
+  LockedAmount = GetAssetLockedAmount()
+  data.table( Currency,CurrencyId,Amount,FreeAmount,LockedAmount)
+}
+#' Get Asset field
+GetAssetCurrency<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAssetCurrency')
+}
+#' Get Asset field
+GetAssetCurrencyId<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAssetCurrencyId')
+}
+#' Get Asset field
+GetAssetAmount<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAssetAmount')
+}
+#' Get Asset field
+GetAssetFreeAmount<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAssetFreeAmount')
+}
+#' Get Asset field
+GetAssetLockedAmount<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAssetLockedAmount')
+}
---
+++ R/R/Connect.R
@@ -0,0 +1,35 @@
+#' Initialize the CLR runtime and loads QuoteHistory host assembly
+#'
+ttmInit <- function() {
+  require(rClr)
+  if(!require(stringi))
+  {
+    install.packages("stringi")
+    require(stringi)
+  }
+  if(!require(data.table))
+  {
+    install.packages("data.table")
+    require(data.table)
+  }
+  fileName <-system.file("data", "rTTManApi.dll", package="rTTManApi")
+  clrLoadAssembly(fileName)
+}
+#' Connects to a TT server
+#'
+#' @param address Url of the server
+#' @param login account you login
+#' @param password password of the account
+#' @export
+ttmConnect <- function(address = "",login = "",password = "") {
+  ttmInit()
+  hResult = rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'Connect', address, login,password)
+}
+
+
+#' Disconnect from a TT server
+#'
+#' @export
+ttmDisconnect <- function() {
+  hResult = rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'Disconnect')
+}
