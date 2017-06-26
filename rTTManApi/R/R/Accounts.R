d43f829921e4f9ad546e331ca4a06b80531c6267
Yahor Mazko <yahor.mazko@soft-fx.lv>
2017-06-26 16:53:55 +0300

    Move sources to rTTManApi/ folder

---
+++ rTTManApi/R/R/Accounts.R
@@ -0,0 +1,149 @@
+#' Gets the Accounts as requested
+#'
+#' @export
+ttmGetAllAccounts <- function() {
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAllAccounts')
+  GetAccountFrame()
+}
+#' Get Account table
+GetAccountFrame<-function()
+{
+  Login = GetAccountLogin()
+  Name = GetAccountName()
+  Domain = GetAccountDomain()
+  Group = GetAccountGroup()
+  AccountingType = GetAccountingType()
+  Blocked = GetAccountBlocked()
+  Readonly = GetAccountReadonly()
+  Leveage = GetAccountLeveage()
+  Profit = GetAccountProfit()
+  Comission = GetAccountComission()
+  AgentComission = GetAccountAgentComission()
+  Swap = GetAccountSwap()
+  Equity = GetAccountEquity()
+  Margin = GetAccountMargin()
+  MarginLevel = GetAccountMarginLevel()
+  Balance = GetAccountBalance()
+  BalanceCurrency = GetAccountBalanceCurrency()
+  MarginCallLevel = GetAccountMarginCallLevel()
+  StopOutLevel = GetAccountStopOutLevel()
+  IsValid = GetAccountIsValid()
+  IsWebApiEnabled = GetAccountIsWebApiEnabled()
+  IsTwoFactorAuthSet = GetAccountIsTwoFactorAuthSet()
+  IsArchived = GetAccountIsArchived()
+  FeedPriority = GetAccountFeedPriority()
+  Version = GetAccountVersion()
+  MarginFree = GetAccountMarginFree()
+  TotalComission = GetAccountTotalComission()
+  data.table(Login,Name,Domain,Group,AccountingType,Blocked,Readonly,Leveage,Profit,Comission,AgentComission,TotalComission,Swap,
+  Equity,Margin,MarginLevel,Balance,BalanceCurrency,MarginCallLevel,MarginFree,StopOutLevel,IsValid,IsWebApiEnabled,
+  IsTwoFactorAuthSet,IsArchived,FeedPriority,Version)
+}
+#' Get Account field
+GetAccountLogin<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountLogin')
+}
+#' Get Account field
+GetAccountName<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountName')
+}
+#' Get Account field
+GetAccountDomain<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountDomain')
+}
+#' Get Account field
+GetAccountGroup<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountGroup')
+}
+#' Get Account field
+GetAccountingType<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountingType')
+}
+#' Get Account field
+GetAccountBlocked<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetBlocked')
+}
+#' Get Account field
+GetAccountReadonly<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetReadonly')
+}
+#' Get Account field
+GetAccountLeveage<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetLeveage')
+}
+#' Get Account field
+GetAccountProfit<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetProfit')
+}
+#' Get Account field
+GetAccountComission<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetComission')
+}
+#' Get Account field
+GetAccountAgentComission<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAgentComission')
+}
+#' Get Account field
+GetAccountSwap<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetSwap')
+}
+#' Get Account field
+GetAccountEquity<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetEquity')
+}
+#' Get Account field
+GetAccountMargin<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetMargin')
+}
+#' Get Account field
+GetAccountMarginLevel<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetMarginLevel')
+}
+#' Get Account field
+GetAccountBalance<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetBalance')
+}
+#' Get Account field
+GetAccountBalanceCurrency<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetBalanceCurrency')
+}
+#' Get Account field
+GetAccountMarginCallLevel<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetMarginCallLevel')
+}
+#' Get Account field
+GetAccountStopOutLevel<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountStopOutLevel')
+}
+#' Get Account field
+GetAccountIsValid<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetIsValid')
+}
+#' Get Account field
+GetAccountIsWebApiEnabled<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetAccountIsWebApiEnabled')
+}
+#' Get Account field
+GetAccountIsTwoFactorAuthSet<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetIsTwoFactorAuthSet')
+}
+#' Get Account field
+GetAccountIsArchived<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetIsArchived')
+}
+#' Get Account field
+GetAccountFeedPriority<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetFeedPriority')
+}
+#' Get Account field
+GetAccountVersion<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetVersion')
+}
+#' Get Account field
+GetAccountMarginFree<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetMarginFree')
+}
+#' Get Account field
+GetAccountTotalComission<-function(){
+  rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'GetComission')
+}
---
+++ rTTManApi/TTManagerAPI2R/rTTManApi/Logger.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+
+namespace rTTManApi
+{
+    public static class Logger
+    {
+        private static ILog log = LogManager.GetLogger("Logger");
+        private static bool _isInit;
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            if (_isInit) return;
+            _isInit = true;
+            Hierarchy hierarchy = (Hierarchy) LogManager.GetRepository();
+
+            PatternLayout patternLayout = new PatternLayout
+            {
+                ConversionPattern = "%date [%thread] %-5level %logger - %message%newline"
+            };
+            patternLayout.ActivateOptions();
+
+            PatternLayout consoleLayout = new PatternLayout {ConversionPattern = "%message%newline"};
+            consoleLayout.ActivateOptions();
+
+            ConsoleAppender console = new ConsoleAppender {Layout = consoleLayout};
+            console.ActivateOptions();
+            hierarchy.Root.AddAppender(console);
+
+            RollingFileAppender roller = new RollingFileAppender
+            {
+                AppendToFile = true,
+                File = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) +
+                       string.Format(@"\rTTManApi\Logs\{0}-{1}-{2} Log.txt", DateTime.Now.Year,
+                           DateTime.Now.Month,
+                           DateTime.Now.Day),
+                Layout = patternLayout,
+                MaxSizeRollBackups = 5,
+                MaximumFileSize = "1GB",
+                RollingStyle = RollingFileAppender.RollingMode.Size,
+                StaticLogFileName = true
+            };
+            roller.ActivateOptions();
+            hierarchy.Root.AddAppender(roller);
+
+            MemoryAppender memory = new MemoryAppender();
+            memory.ActivateOptions();
+            hierarchy.Root.AddAppender(memory);
+
+            hierarchy.Root.Level = Level.Info;
+            hierarchy.Configured = true;
+        }
+    }
+}
