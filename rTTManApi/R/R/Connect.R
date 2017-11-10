1b3b07c151e0c4f53af2505e81c7ef20ca3b156a
Yahor Mazko <yahor.mazko@soft-fx.lv>
2017-11-10 17:58:27 +0300

    Add reading credentials for connecting from file

--- rTTManApi/R/R/Connect.R
+++ rTTManApi/R/R/Connect.R
@@ -7,6 +7,11 @@ ttmInit <- function() {
     install.packages("stringi")
     require(stringi)
   }
+  if(!require(properties))
+  {
+    install.packages("properties")
+    require(properties)
+  }
   if(!require(data.table))
   {
     install.packages("data.table")
@@ -20,9 +25,18 @@ ttmInit <- function() {
 #' @param address Url of the server
 #' @param login account you login
 #' @param password password of the account
+#' @param file when not empty, credentials will read from it
 #' @export
-ttmConnect <- function(address = "",login = "",password = "") {
+ttmConnect <- function(address = "",login = "",password = "", file = "") {
   ttmInit()
+  if(nchar(file)!=0)
+  {
+    configFilePath = file.path(getwd(),file)
+    data = read.properties(configFilePath)
+    address = data$server
+    login = data$login
+    password = data$password
+  }
   hResult = rClr::clrCallStatic('rTTManApi.rTTManApiHost', 'Connect', address, login,password)
   if(hResult != 0) stop('Unable to connect')
 }
